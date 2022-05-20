a8d60795424c43e4174fac89185c820ca56de2af
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2022-05-20 00:09:49 -0700

    IP Updates

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,17 +4,15 @@ import random
 # Tic Tac Toe, Three in a Row!
 class TicTacToe:
     """Play a TicTacToe game!"""
+    # initialize the game with our board as an empty list 
     def __init__(self):
-        # Initialize game with the board as an empty list 
         self.board = []
-
-
-    
+    # method to create the gameboard
     def create_board(self):
         for i in range(9):
             self.board.append(i + 1)
         self.board = np.reshape(self.board, (3, 3))
-
+    # method to display the gameboard
     def display_board(self):
         print('-------------------------------')
         for row in self.board:
@@ -25,23 +23,31 @@ class TicTacToe:
             print()
             print('|         |         |         |')
             print('-------------------------------')
-
-        
-
-
-
-
-
-
-
-
-
-
+    # method to enable the player to choose to be Xs or Os 
+    def choose_player_marker(self):
+        marker = ' '
+        while (marker != 'X' and marker != 'O'):
+            marker = input("Do you want to be Xs or Os? ").upper()
+        if marker == 'X':
+            return ['X', 'O']
+        else:
+            return ['O', 'X']
+    # method to assign player markers in PvP mode
+    def assign_PvP_markers(self):
+        player_1, player_2 = self.choose_player_marker()
+        return player_1, player_2
+    # method to assign player markers in PvE modes
+    def assign_PvE_markers(self):
+        player_1, player_2 = self.choose_player_marker()
+        player, computer = player_1, player_2
+        return player, computer
+    # method to randomly decide who goes first
     def coin_flip(self):
-        # initialize player markers
-        player, opponent = "X", "O"
+        player = self.player_1 
+
         # flip the coin
         result = random.randint(0, 1)
+
         if result == 0:
             return player
         elif result == 1: 
